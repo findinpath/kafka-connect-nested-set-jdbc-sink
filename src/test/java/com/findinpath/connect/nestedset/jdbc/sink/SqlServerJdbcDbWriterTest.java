a446f521d4458359b965c72c39062481537e03df
findinpath <findinpath@gmail.com>
2020-05-18 09:26:52 +0200

    added support for mssql server

---
+++ src/test/java/com/findinpath/connect/nestedset/jdbc/sink/SqlServerJdbcDbWriterTest.java
@@ -0,0 +1,27 @@
+package com.findinpath.connect.nestedset.jdbc.sink;
+
+import org.testcontainers.containers.MSSQLServerContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class SqlServerJdbcDbWriterTest extends JdbcDbWriterTest {
+    @Container
+    protected static MSSQLServerContainer msSqlContainer = new MSSQLServerContainer<>()
+            .withInitScript("sink/postgres/init_jdbcdbdriver_mssql.sql");
+
+    @Override
+    protected String getJdbcUrl() {
+        return msSqlContainer.getJdbcUrl();
+    }
+
+    @Override
+    protected String getJdbcUsername() {
+        return msSqlContainer.getUsername();
+    }
+
+    @Override
+    protected String getJdbcPassword() {
+        return msSqlContainer.getPassword();
+    }
+}
